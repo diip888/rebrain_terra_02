fa2c4133f59a0b7869d8e2b7d407c5d4d9c6bc95
dmitry plyushchev <d_plus@mail.ru>
2023-04-27 11:04:29 +0300

    change resources file key rebrain

--- output.tf
+++ output.tf
@@ -1,6 +1,7 @@
 ## OUTPUTS
 output "re_ssh_key" {
-  value = data.digitalocean_ssh_keys.re_key
+#  value = data.digitalocean_ssh_keys.re_key
+  value = data.digitalocean_ssh_key.re_key
 }
 
 output "my_ssh_key" {
--- resources.tf
+++ resources.tf
@@ -1,15 +1,20 @@
 # Rebrain key from data
-data "digitalocean_ssh_keys" "re_key" {
-  filter {
-    key = "name"
-    values = ["REBRAIN.SSH.PUB.KEY"]
-  }
+# DATA Sources
+#data "digitalocean_ssh_keys" "re_key" {
+#  filter {
+#    key = "name"
+#    values  = ["REBRAIN.SSH.PUB.KEY"]
+#  }
+#}
+
+data "digitalocean_ssh_key" "re_key" {
+  name = "REBRAIN.SSH.PUB.KEY"
 }
 
 ##  RESOURCES ###########################################
 
 resource "digitalocean_ssh_key" "dplusmailru" {
-  name       = var.name_ssh_key #"ssh.dplusmailru"
+  name       = var.name_ssh_key
   public_key = file(var.ssh_path_public_file)
 }
 
@@ -18,8 +23,6 @@ resource "digitalocean_droplet" "webdo-dplus" {
   name     = var.name-resource
   region   = var.region
   size     = var.size
-  ssh_keys = [digitalocean_ssh_key.dplusmailru.id]
-#  ssh_keys = [digitalocean_ssh_key.dplusmailru.id, data.digitalocean_ssh_keys.re_key.id]
-#
+  ssh_keys = [digitalocean_ssh_key.dplusmailru.id, data.digitalocean_ssh_key.re_key.id]
   tags     = [digitalocean_tag.mytag1.id, digitalocean_tag.mytag2.id]
 }
